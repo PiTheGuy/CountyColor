4d9988f93e941f3d56d6c0731d16b2bd7ae96081
PiTheGuy <the4mooresrock@gmail.com>
2025-04-15 11:28:00 -0500

    Fixes

--- core/src/main/java/pitheguy/countycolor/render/renderer/CountyLevelRenderer.java
+++ core/src/main/java/pitheguy/countycolor/render/renderer/CountyLevelRenderer.java
@@ -17,7 +17,6 @@ import static pitheguy.countycolor.render.util.RenderConst.RENDER_SIZE;
 public abstract class CountyLevelRenderer extends RegionRenderer {
 
     protected Map<String, CountyData.County> counties;
-    private final List<String> independentCities = new ArrayList<>();
 
     protected void renderRegion(OrthographicCamera camera, boolean thick, boolean scaleThickness) {
         super.renderRegion(counties.values().stream().map(CountyData.County::getPolygons).toList(), camera, thick, scaleThickness);
@@ -31,7 +30,7 @@ public abstract class CountyLevelRenderer extends RegionRenderer {
             .filter(CountyData.County::isIndependentCity)
             .forEach(county -> {
                 if (!predicate.test(county)) return;
-                if (county.getPolygons().isVisibleToCamera(camera)) return;
+                if (!county.getPolygons().isVisibleToCamera(camera)) return;
                 if (thick) renderThickSubregionOutline(county.getPolygons(), scaleThickness ? OUTLINE_THICKNESS * camera.zoom : OUTLINE_THICKNESS);
                 else renderSubregionOutline(county.getPolygons());
             });
@@ -42,17 +41,14 @@ public abstract class CountyLevelRenderer extends RegionRenderer {
         return counties.entrySet().stream()
             .sorted(Map.Entry.comparingByValue(Comparator.comparing(CountyData.County::isIndependentCity).reversed()))
             .filter(entry -> {
-                PolygonCollection subregion = entry.getValue().getPolygons();
-                if (!subregion.boundsCheck(coordinate)) return false;
-                for (List<Vector2> polygon : subregion.getPolygons())
-                    if (RenderUtil.pointInPolygon(coordinate.cpy().scl(2f / RENDER_SIZE), polygon))
+                PolygonCollection county = entry.getValue().getPolygons();
+                if (!county.boundsCheck(coordinate)) return false;
+                Vector2 scaledCoordinate = coordinate.cpy().scl(2f / RENDER_SIZE);
+                for (List<Vector2> polygon : county.getPolygons())
+                    if (RenderUtil.pointInPolygon(scaledCoordinate, polygon))
                         return true;
                 return false;
             }).findFirst().map(Map.Entry::getValue).orElse(null);
-    }
-
-    public boolean isIndependentCity(String name) {
-        return independentCities.contains(name);
     }
 
 }
